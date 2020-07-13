7bd0fa97df409ded0a34d66dd887845897bf548f
Enol Fernandez <enol.fernandez@egi.eu>
2020-07-13 10:44:41 +0100

    pylint shut up

--- glite_info_update_endpoints/__init__.py
+++ glite_info_update_endpoints/__init__.py
@@ -101,6 +101,7 @@ def get_url_data(url, capath, cafile):
     # is no longer possible to download HTTPS data without having server
     # CA certificate in trusted store or explicitely disable verification.
     if hasattr(ssl, 'create_default_context'):
+        # pylint: disable=protected-access
         if capath is not None or cafile is not None:
             context = ssl.create_default_context(cafile=cafile, capath=capath)
         else:
